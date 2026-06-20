f17d947227f35faf19c1fa9e3bc88d932f1cc880
cmwillett <craig.willett@gmail.com>
2026-06-20 14:43:34 -0400

    adding log

--- src/pages/scoreboard.js
+++ src/pages/scoreboard.js
@@ -5,6 +5,7 @@ export async function renderScoreboard() {
   try {
     const result = await getAvailableGames('ALL');
     const games = result.data || [];
+    console.log('FIRST GAME:', games[0]);
 
     return `
       <div class="page-header">
